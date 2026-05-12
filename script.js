f92c40e87fa49191c53a85ad242a99c4ccf06d34
hanif-s43 <aahanip55@gmail.com>
2026-05-12 13:20:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwYKMJAAintpER13opkii0nb2SbrZCEbpgr9CmlhiNEKDDfsgYUjoYEuu0KK-0QlAOp/exec';
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx2xQYeE2JaBU4mEv762WSg3qBGTYnw1CUryUIh7XGvRHxML8MDkr3xF50OYvAcmMos/exec";
 
 async function fetchLaporan() {
     const loading = document.getElementById('loading');
